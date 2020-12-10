baaac6578004415bc4c8d3b1025b6915b65958fe
Bruno Petinati <brunopetinati@hotmail.com>
2020-12-10 20:06:31 -0300

    feat: register, style, refactoring and components

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,29 +1,22 @@
-import {
-  Container,
-  RotateDiv,
-  Title,
-  HeaderButton1,
-  HeaderButton2,
-  SeeImage,
-} from "./styles";
 import image from "../../assets/images/register.svg";
-/* import register from "../../assets/images"; */
 
 //COMPONENTS
 import RegisterForm from "../../components/RegisterForm";
+
+//STYLES
+import { Container, BoxPrimary, BoxSecondary } from "./styles";
 
 const Register = () => {
   return (
     <>
       <Container>
-        <HeaderButton1>Login </HeaderButton1>
-        <HeaderButton2>Conheça nossos Devs</HeaderButton2>
-        <RotateDiv>
-          <SeeImage src={image} alt="Register Now!" />
-        </RotateDiv>
-
-        <Title>Register</Title>
-        <RegisterForm />
+        <BoxPrimary>
+          <img src={image} alt="Register Now!" width="400px" />
+        </BoxPrimary>
+        <BoxSecondary>
+          <h1>Register</h1>
+          <RegisterForm />
+        </BoxSecondary>
       </Container>
     </>
   );
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,98 +1,37 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: relative;
-  width: 100vw;
+  width: 90%;
   height: 100vh;
   overflow: hidden;
-`;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -50px;
+    height: 300%;
+    width: 70%;
+    background-color: #4469e0;
+    z-index: -1;
 
-export const RotateDiv = styled.div`
-  position: absolute;
-  background: #1172d7;
-  height: 1100px;
-  width: 1900px;
-  top: -460px;
-  left: -1170px;
-  transform: rotate(25deg);
-`;
-
-export const HeaderButton1 = styled.div`
-  background: #fff0;
-  border: none;
-  cursor: pointer;
-  position: absolute;
-  left: 1060px;
-  outline: none;
-  top: 0px;
-  font-size: 15px;
-  border-bottom: solid black 2px;
-  padding: 1px 0px;
-  margin: 1px 6px;
-  :hover {
-    padding: 1px 6px;
-    margin: 1px 0px;
-    border-bottom: solid black 2px;
-    transition: 0.4s;
-  }
-`;
-export const HeaderButton2 = styled.button`
-  background: #fff0;
-  border: none;
-  cursor: pointer;
-  position: absolute;
-  left: 1120px;
-  outline: none;
-  top: 0px;
-  font-size: 15px;
-  border-bottom: solid black 2px;
-  padding: 1px 0px;
-  margin: 1px 6px;
-  :hover {
-    padding: 1px 6px;
-    margin: 1px 0px;
-    border-bottom: solid black 2px;
-    transition: 0.4s;
+    transform: rotate(26deg);
+    transform-origin: 100% 0;
   }
 `;
 
-export const Title = styled.div`
-  position: absolute;
-  left: 880px;
-  top: 150px;
-  text-align: center;
-  color: #383d3d;
-  font-style: normal;
-  font-size: 32px;
-  letter-spacing: 0px;
+export const BoxPrimary = styled.div`
+  width: 40%;
+  display: flex;
+  justify-content: flex-start;
 `;
 
-export const Form = styled.div`
-  position: absolute;
-  background: white;
-  width: 300px;
-  height: 450px;
-  left: 820px;
-  top: 230px;
-  text-align: center;
-  color: red;
-  font-size: 12px;
-  font-style: italic;
-  input {
-    margin: 3px;
-    padding: 2px;
-  }
-`;
-
-export const SmallMargin = styled.div`
-  margin-top: 1px;
-  margin-bottom: 1px;
-`;
-
-export const SeeImage = styled.img`
-  position: absolute;
-  right: 130px;
-  top: 350px;
-  width: 450px;
-  transform: rotate(-25deg);
+export const BoxSecondary = styled(BoxPrimary)`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
