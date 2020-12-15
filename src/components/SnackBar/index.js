5a00b1daf6ece566cd8360ce77ccc4f957edb609
@NunesRenan <renannunes2008@hotmail.com>
2020-12-15 15:30:19 -0300

    chore: code refactored

--- src/components/SnackBar/index.js
+++ src/components/SnackBar/index.js
@@ -1,14 +1,11 @@
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
-import { makeStyles } from "@material-ui/core/styles";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 const ButtonSnackbar = ({ open, severityValue, message }) => {
-  const classes = useStyles();
-
   const snackAlert = (severityValue, message) => {
     return <Alert severity={severityValue}>{message}</Alert>;
   };
