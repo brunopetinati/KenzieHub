cee5a02e310fb6b5b13cce209597d34d153139c5
[Rafael Assad] <[rafael.assad1010@gmail.com]>
2020-12-16 18:08:50 -0300

    chore: removed commented lines from store/index that will not use anymore

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,11 @@ import techsReducer from "./Modules/Techs/reducer";
 import worksReducer from "./Modules/Works/reducer";
 import dataReducer from "./Modules/Data/reducer";
 import userReducer from "./Modules/User/reducer";
-// import userDataReducer from "./Modules/UserData/reducer";
 import authenticateReducer from "./Modules/Authenticated/reducer";
 
 const reducers = combineReducers({
   techs: techsReducer,
   user: userReducer,
-  // userData: userDataReducer,
   works: worksReducer,
   data: dataReducer,
   authenticated: authenticateReducer,
